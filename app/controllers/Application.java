f99157accf7af6c68f40c8a14f16e3f62352ee77
Lincoln Stoll <l@lds.li>
2010-05-08 15:37:59 +0200

    Adding simple OpenID Auth

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import play.mvc.*;
+import play.libs.OpenID;
+import play.libs.OpenID.*;
 
 public class Application extends Controller {
 
@@ -8,4 +10,21 @@ public class Application extends Controller {
         render();
     }
 
+    public static void login() {
+        render();
+    }
+    
+    public static void authenticate(String user) {
+        if(OpenID.isAuthenticationResponse()) {
+            UserInfo verifiedUser = OpenID.getVerifiedID();
+            if(verifiedUser == null) {
+                flash.put("error", "Oops. Authentication has failed");
+                login();
+            } 
+            session.put("user", verifiedUser.id);
+            index();
+        } else {
+            OpenID.id(user).verify(); // will redirect the user
+        }
+    }   
 }
