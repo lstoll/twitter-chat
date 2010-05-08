24dad1ed8b00f063eed1b31e3b361d847278bf4b
Lincoln Stoll <l@lds.li>
2010-05-08 22:28:26 +0200

    Convert to POST, clean formatting

--- streamer/server.js
+++ streamer/server.js
@@ -1,12 +1,12 @@
-var sys = require('sys');
-var http = require('http');
-var base64 = require('./vendor/base64'); 
+var sys = require('sys'),
+    http = require('http'),
+    base64 = require('./vendor/base64'); 
 
 // Command line args 
 var USERNAME = process.ARGV[2];
 var PASSWORD = process.ARGV[3];
 var SERVER = process.ARGV[4];
-      
+
 if (!USERNAME || !PASSWORD || !SERVER)
   return sys.puts("Usage: node server.js <twitter_username> <twitter_password> " + 
                   "<master server>");
@@ -15,17 +15,20 @@ var auth = base64.encode(USERNAME + ':' + PASSWORD);
 
 // Connection to Twitter's streaming API
 var twitter = http.createClient(80, "stream.twitter.com");
-var request = twitter.request("GET", "/1/statuses/filter.json?track=Bieber",
+var request = twitter.request("POST", "/1/statuses/filter.json",
                               {'host': 'stream.twitter.com', 
-                               'Authorization': auth });
+                               'Authorization': auth,
+                               'Content-Type': 'application/x-www-form-urlencoded'});
 
 
 request.addListener('response', function (response) {
-sys.puts('STATUS: ' + response.statusCode);
+  sys.puts('STATUS: ' + response.statusCode);
   response.setBodyEncoding("utf8");
   response.addListener("data", function (chunk) {
-                        // Send response to all connected clients
-                         sys.puts(chunk);
+    // Send response to all connected clients
+    sys.puts('--------------------------------------------');
+    sys.puts(chunk);
   });
 });
+request.write("track=Bieber");
 request.end();
