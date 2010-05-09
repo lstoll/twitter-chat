125b051c6d601383e6a2a5565da0ed92ecd83184
Lincoln Stoll <l@lds.li>
2010-05-09 21:29:26 +0200

    Moving auth into it's own controller

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,30 +1,10 @@
 package controllers;
 
 import play.mvc.*;
-import play.libs.OpenID;
-import play.libs.OpenID.*;
 
 public class Application extends Controller {
 
     public static void index() {
         render();
     }
-
-    public static void login() {
-        render();
-    }
-    
-    public static void authenticate(String user) {
-        if(OpenID.isAuthenticationResponse()) {
-            UserInfo verifiedUser = OpenID.getVerifiedID();
-            if(verifiedUser == null) {
-                flash.put("error", "Oops. Authentication has failed");
-                login();
-            } 
-            session.put("user", verifiedUser.id);
-            index();
-        } else {
-            OpenID.id(user).verify(); // will redirect the user
-        }
-    }   
 }
