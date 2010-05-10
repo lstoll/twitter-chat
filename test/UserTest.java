fc5c907bccf304ebc586ee72fec8b22c31e81a91
Lincoln Stoll <l@lds.li>
2010-05-10 20:03:30 +0200

    Tests for User

--- test/UserTest.java
+++ test/UserTest.java
@@ -7,6 +7,7 @@ import models.*;
 
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import java.util.Date;
 
 public class UserTest extends UnitTest {
 
@@ -24,19 +25,90 @@ public class UserTest extends UnitTest {
     }
 
     @Test
-    public void createAndGetUserByOpenID() {
-        User u = new User();
-        u.openID = "http://google.com";
+    public void createAndGetUserByXMPPID() {
+        User u = new User("l@lds.li");
         u.insert();
 
         // now get the user
-        User getUser = User.byOpenID("http://google.com");
+        User getUser = User.byOpenID("l@lds.li");
         assertNotNull(getUser);
-        assertEquals(getUser.openID, u.openID);
+        assertEquals(getUser.xmppID, u.xmppID);
 
         // Makes sure we aren't leaking - will fail on subsequent runs
         DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
         assertEquals(1, ds.prepare(new Query("User")).countEntities());
+    }
+
+    @Test
+    public void userShouldNotBeRegisteredWihoutXMPPIDandOauth() {
+        User u = new User("l@lds.li");
+        u.insert();
+
+        assertFalse(u.isActive());
+
+        u.twitterToken = "aa";
+        u.twitterTokenSecret = "xx";
+
+        u.update();
+
+        assertTrue(u.isActive());
+    }
+
+    @Test
+    public void needsOauthReminder() {
+        // if isn't active and hasn't been prompted in the last hour, re-prompt
+        User u = new User("l@lds.li");
+        u.insert();
+
+        // we need to be reminded
+        assertEquals(1, User.needsReminder().size());
+
+        // set their last remind time to now.
+        u.lastReminded = new Date();
+
+        // should no longer need reminder
+        assertEquals(0, User.needsReminder().size());
+    }
+
+    @Test
+    public void addRemoveWatchToExistingUser() {
+        DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+        assertEquals(0, ds.prepare(new Query("Watch")).countEntities());
+
+        User u = new User("l@lds.li");
+        u.insert();
+
+        u.addWatch("testterm");
+
+        assertEquals(1, ds.prepare(new Query("Watch")).countEntities());
+        assertEquals("testterm", Watch.all(Watch.class).get().term);
+
+        u.removeWatch("testterm");
+
+        assertEquals(0, ds.prepare(new Query("Watch")).countEntities());
+    }
+
+    @Test
+    public void addRemoveWatchExisting() {
+        DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+        assertEquals(0, ds.prepare(new Query("Watch")).countEntities());
+
+        User u1 = new User("1@u.com");
+        u1.insert();
+        User u2 = new User("2@u.com");
+        u2.insert();
+
+        u1.addWatch("term");
+        assertEquals(1, ds.prepare(new Query("Watch")).countEntities());
+
+        u2.addWatch("term");
+        assertEquals(1, ds.prepare(new Query("Watch")).countEntities());
+
+        u2.removeWatch("term");
+        assertEquals(1, ds.prepare(new Query("Watch")).countEntities());
+
+        u1.removeWatch("term");
+        assertEquals(0, ds.prepare(new Query("Watch")).countEntities());
     }
 
 }
