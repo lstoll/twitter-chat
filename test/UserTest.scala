e80e97c874278ab29aff6bafe8e11fab6f276129
Lincoln Stoll <l@lds.li>
2010-05-12 20:35:59 +0200

    Covert UserTest to Scala

---
+++ test/UserTest.scala
@@ -0,0 +1,114 @@
+import play.test._
+
+import org.junit._
+import org.scalatest.junit._
+import org.scalatest._
+import org.scalatest.matchers._
+import com.google.appengine.tools.development.testing._
+import com.google.appengine.api.datastore._
+
+import models._
+
+class UserTest extends UnitTest with ShouldMatchersForJUnit {
+  var helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig)
+
+  @Before def setUp = {
+    helper.setUp
+  }
+
+  @After def tearDown = {
+    helper.tearDown
+  }
+
+  @Test def createAndGetUserByXMPPID() {
+    var u = new User()
+    u.xmppID = "l@lds.li"
+    u.insert
+
+    // now get the user
+    var getUser = User.byXmppId("l@lds.li")
+    
+    getUser should not be (null)
+    getUser.xmppID should equal (u.xmppID)
+
+    // Makes sure we aren't leaking - will fail on subsequent runs
+    var ds = DatastoreServiceFactory.getDatastoreService
+    ds.prepare(new Query("User")).countEntities should equal (1)
+  }
+
+
+  @Test def userShouldNotBeRegisteredWihoutXMPPIDandOauth {
+    var u = new User()
+    u.xmppID = "l@lds.li"
+    u.insert
+
+    u.isActive should be (false)
+
+    u.twitterToken = "aa"
+    u.twitterTokenSecret = "xx"
+
+    u.update
+
+    u.isActive should be (true)
+  }
+
+  @Test def needsOauthReminder {
+    // if isn't active and hasn't been prompted in the last hour, re-prompt
+    var u = new User()
+    u.xmppID = "l@lds.li"
+    u.insert
+
+    // we need to be reminded
+    User.needsReminder().size should equal (1)
+
+    // set their last remind time to now.
+    u.lastReminded = new java.util.Date()
+    u.update
+
+    // should no longer need reminder
+    User.needsReminder().size should equal (0)
+  }
+
+  @Test def addRemoveWatchToExistingUser {
+    var ds = DatastoreServiceFactory.getDatastoreService
+    ds.prepare(new Query("Watch")).countEntities should equal (0)
+
+    var u = new User()
+    u.xmppID = "l@lds.li"
+
+    u.insert
+
+    u.addWatch("testterm")
+
+    ds.prepare(new Query("Watch")).countEntities should equal (1)
+    Watch.all.get.getTerm should equal ("testterm")
+
+    u.removeWatch("testterm")
+
+    ds.prepare(new Query("Watch")).countEntities should equal (0)
+  }
+
+  @Test def addRemoveWatchExisting {
+    var ds = DatastoreServiceFactory.getDatastoreService
+    ds.prepare(new Query("Watch")).countEntities should equal (0)
+
+    var u1 = new User()
+    u1.xmppID = "1@u.com"
+    u1.insert
+    var u2 = new User()
+    u2.xmppID = "2@u.com"
+    u2.insert
+
+    u1.addWatch("term")
+    ds.prepare(new Query("Watch")).countEntities should equal (1)
+
+    u2.addWatch("term")
+    ds.prepare(new Query("Watch")).countEntities should equal (1)
+
+    u2.removeWatch("term")
+    ds.prepare(new Query("Watch")).countEntities should equal (1)
+
+    u1.removeWatch("term");
+    ds.prepare(new Query("Watch")).countEntities should equal (0)
+  }
+}
