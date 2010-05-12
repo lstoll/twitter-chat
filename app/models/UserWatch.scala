b938f4f130d7704c97eaaad5c48ed21a05602bef
Lincoln Stoll <l@lds.li>
2010-05-12 16:21:03 +0200

    Adding scala model implementations

---
+++ app/models/UserWatch.scala
@@ -0,0 +1,32 @@
+package models
+
+import reflect.BeanProperty
+import siena._
+
+@Table("UserWatch")
+class UserWatch extends Model {
+
+  @Id
+  @BeanProperty
+  var id: Long = -1
+
+  @Column(Array("user_id"))
+  @Max(200) @NotNull
+  @BeanProperty
+  var userId: Long = -1
+
+  @Column(Array("watch_id"))
+  @Max(200) @NotNull
+  @BeanProperty
+  var watchId: Long = -1
+}
+
+object UserWatch extends Model {
+  def byUserAndWatch(u:User, w:Watch):UserWatch = {
+    Model.all(classOf[UserWatch]).filter("userId", u.getId()).filter("watchId", w.id).get
+  }
+
+  def byWatch(w:Watch):java.util.List[UserWatch] = {
+    Model.all(classOf[UserWatch]).filter("watchId", w.getId).fetch
+  }
+}
---
+++ app/models/Watch.scala
@@ -0,0 +1,47 @@
+package models
+
+import siena._
+import reflect.BeanProperty
+import java.util.ArrayList
+import scala.collection.JavaConversions._
+
+@Table("Watch")
+class Watch extends Model {
+
+  @Id
+  @BeanProperty
+  var id:Long = -1
+
+  @Column(Array("term"))
+  @Max(200) @NotNull
+  @BeanProperty
+  var term: String = null
+
+
+  def users():java.util.List[User] = {
+    var users = new ArrayList[User];
+    var userWatches = Model.all(classOf[UserWatch]).filter("watchId", this.id).fetch
+    userWatches.foreach { uw =>
+      users.add(Model.all(classOf[User]).filter("id", uw.getId).get)
+    }
+    users
+  }
+}
+
+object Watch {
+
+  def findOrCreate(term:String):Watch = {
+        var w = byTerm(term);
+        if (w == null) {
+          w = new Watch()
+          w.term = term
+          w.insert
+        }
+        return w
+    }
+
+  def byTerm(term: String):Watch = {
+    Model.all(classOf[Watch]).filter("term", term).get
+  }
+
+}
