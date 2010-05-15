e1d4b9cae4021165ebbcada23a24a3a827d0cd30
Lincoln Stoll <l@lds.li>
2010-05-15 15:55:38 +0200

    Register page + invite user

--- app/controllers/Register.scala
+++ app/controllers/Register.scala
@@ -1,7 +1,26 @@
 package controllers
 
 import play.mvc.Controller
+import play.data.validation._
+
+import models._
+import utils._
 
 object Register extends Controller {
 
+  def submitJid(@Email @Required jid:String) {
+    if (Validation.hasErrors()) {
+      flash.error("JID Missing or invalid format")
+      Application.index
+    }
+    // Add them to the DB
+    var u = new User
+    u.xmppID = jid
+    u.insert
+    // invite
+    XMPPSend.inviteUser(jid)
+    
+    // Render welcome page
+    render()
+  }
 }
--- app/utils/XMPPSend.scala
+++ app/utils/XMPPSend.scala
@@ -12,47 +12,54 @@ import com.google.appengine.api.xmpp.XMPPServiceFactory;
  * @author lstoll
  */
 object XMPPSend {
-    var testService:XMPPService = null
-
-    def testSetUp = {
-        testService = new TestXMPPService
+  var testService:XMPPService = null
+  
+  def testSetUp = {
+    testService = new TestXMPPService
+  }
+  
+  def testTearDown = {
+    testService = null;
+  }
+  
+  /**
+  * Delivers a XMPP message to the user. Can buffer the message for later if
+  * the user is offline
+  * @param to the JID of the recipient
+  * @param body The message to send
+  * @param buffer true if we should buffer the message if the user is offline
+  * @return true if it was delivered, false if it wasn't (or it was buffered)
+  */
+  def sendMessage(to:String, body:String, buffer:Boolean):Boolean = {
+    var jid = new JID(to)
+    var msg = new MessageBuilder()
+    .withRecipientJids(jid)
+    .withBody(body)
+    .build()
+    
+    var messageSent = false
+    var xmpp = getCurrentXMPPService
+    if (xmpp.getPresence(jid).isAvailable) {
+      var status = xmpp.sendMessage(msg)
+      messageSent = (status.getStatusMap().get(jid) == SendResponse.Status.SUCCESS);
     }
-
-    def testTearDown = {
-        testService = null;
+    else {
+      // TODO - add the message to a buffer queue
     }
+    messageSent
+  }
 
-    /**
-     * Delivers a XMPP message to the user. Can buffer the message for later if
-     * the user is offline
-     * @param to the JID of the recipient
-     * @param body The message to send
-     * @param buffer true if we should buffer the message if the user is offline
-     * @return true if it was delivered, false if it wasn't (or it was buffered)
-     */
-    def sendMessage(to:String, body:String, buffer:Boolean):Boolean = {
-        var jid = new JID(to)
-        var msg = new MessageBuilder()
-            .withRecipientJids(jid)
-            .withBody(body)
-            .build()
-
-        var messageSent = false
-        var xmpp = getCurrentXMPPService
-        if (xmpp.getPresence(jid).isAvailable) {
-            var status = xmpp.sendMessage(msg)
-            messageSent = (status.getStatusMap().get(jid) == SendResponse.Status.SUCCESS);
-        }
-        else {
-            // TODO - add the message to a buffer queue
-        }
-        messageSent
-    }
-
-    def getCurrentXMPPService():XMPPService = {
-        if (testService != null)
-          return testService
-        else 
-          return XMPPServiceFactory.getXMPPService()
-    }
+  /**
+  * Send a XMPP invite to the user
+  */
+  def inviteUser(jid:String) = {
+    getCurrentXMPPService.sendInvitation(new JID(jid))
+  }
+  
+  def getCurrentXMPPService():XMPPService = {
+    if (testService != null)
+    return testService
+    else 
+    return XMPPServiceFactory.getXMPPService()
+  }
 }
